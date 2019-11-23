f7a0b9b2f46e299695f0e4434ff98ea0c0e80305
Uzeir1 <58044751+Uzeir1@users.noreply.github.com>
2019-11-23 10:26:19 +0100

    Delete Hello.cs

--- Hello.cs
+++
@@ -1,9 +0,0 @@
-public  class Hello
- { 
-   public  static  void Main ( ) 
-   { 
-	System . Console . WriteLine ( " Hello, World! " ) ; 
-	System . Console . WriteLine ( " Hello, GitHub! " ) ; 
-	System . Console . WriteLine ( " Hello, Local! " ) ; 
-   } 
-}
